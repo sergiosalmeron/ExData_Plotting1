729faee8103789aae58c4bdd229aeab1bf5018b6
sergio <netsergiosm@gmail.com>
2015-04-10 19:35:51 +0200

    Comments added_2

--- plot4.R
+++ plot4.R
@@ -1,6 +1,9 @@
+#load the data
 datos<-read.csv(file = "./household_power_consumption.txt", header = TRUE, na.strings="?",sep = ";")
+#adapt date and time fields to a more appropriate formant
 datos$Time<-strptime(paste(datos$Date, datos$Time, sep = " "), format = "%d/%m/%Y %H:%M:%S")
 datos$Date<-as.Date(datos$Date, format = "%d/%m/%Y")
+#filter the data in order to use only data from "2007-02-01" to "2007-02-02"
 datos<-datos[datos$Date>=as.Date("2007-02-01") & datos$Date<=as.Date("2007-02-02"),]
 
 #To show the days in English
